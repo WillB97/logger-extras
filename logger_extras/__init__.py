ab2297a3a6d3603df678d36e76f5cd548a315f4a
WillB97 <will@rudi.org.uk>
2023-07-29 14:46:52 +0100

    isort

--- logger_extras/__init__.py
+++ logger_extras/__init__.py
@@ -2,11 +2,12 @@
 from ._version import __version__
 from .filters import DiffTimeFilter, RelativeTimeFilter
 from .formatters import TieredFormatter
+from .utils import log_function_call
+
 try:
     from .mqtt import MQTTHandler
 except ImportError:
-    MQTTHandler = None
-from .utils import log_function_call
+    MQTTHandler = None  # type: ignore
 
 __all__ = [
     '__version__',
